48c7abc1b5c5e181b78f229d94f641063189c54c
FlorianMichael <florian.michael07@gmail.com>
2025-08-01 22:15:55 +0200

    Encrypt all files separately

--- src/main/java/de/florianmichael/pit/Pit.java
+++ src/main/java/de/florianmichael/pit/Pit.java
@@ -18,7 +18,9 @@
 
 package de.florianmichael.pit;
 
-import javax.crypto.*;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
@@ -97,21 +99,20 @@ public final class Pit {
                 final String encryptedFile = args[1];
                 final String requestedFile = args.length > 2 ? args[2] : null;
                 try {
-                    final Map<String, byte[]> files = decryptToMemory(new File(encryptedFile), password);
                     if (requestedFile == null) {
-                        System.out.println("Available files:");
-                        files.keySet().forEach(f -> System.out.println(" - " + f));
+                        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(encryptedFile))) {
+                            ZipEntry entry;
+                            System.out.println("Available files:");
+                            while ((entry = zis.getNextEntry()) != null) {
+                                if (!entry.isDirectory()) {
+                                    System.out.println(" - " + entry.getName());
+                                }
+                            }
+                        }
                         return;
                     }
 
-                    if (!files.containsKey(requestedFile)) {
-                        System.err.println("File not found: " + requestedFile);
-                        System.out.println("Available files:");
-                        files.keySet().forEach(f -> System.out.println(" - " + f));
-                        return;
-                    }
-
-                    final byte[] content = files.get(requestedFile);
+                    final byte[] content = decryptSingleFileFromArchive(new File(encryptedFile), requestedFile, password);
                     System.out.println("Contents of " + requestedFile + ":\n");
                     System.out.println(new String(content, StandardCharsets.UTF_8));
                 } catch (final Exception e) {
@@ -164,13 +165,15 @@ public final class Pit {
                 try {
                     Map<String, byte[]> files = Pit.decryptToMemory(new File(args[1]), password);
                     File newFile = new File(args[3]);
-                    if (files.containsKey(args[2])) {
-                        files.put(args[2], Files.readAllBytes(newFile.toPath()));
-                        Pit.encryptFromMemory(files, new File(args[1]), password);
-                        System.out.println("File edited: " + args[2]);
-                    } else {
+                    try {
+                        decryptSingleFileFromArchive(new File(args[1]), args[2], password); // for validation
+                    } catch (FileNotFoundException e) {
                         System.err.println("File not found: " + args[2]);
+                        return;
                     }
+                    files.put(args[2], Files.readAllBytes(newFile.toPath()));
+                    Pit.encryptFromMemory(files, new File(args[1]), password);
+                    System.out.println("File edited: " + args[2]);
                 } catch (Exception e) {
                     System.err.println("Failed to edit file!");
                     throw e;
@@ -203,52 +206,92 @@ public final class Pit {
         }
     }
 
+    private static byte[] decryptEntryContent(final byte[] fullContent, final String password) throws Exception {
+        if (fullContent.length < SALT_LENGTH + 16) {
+            throw new IllegalArgumentException("Encrypted entry is too short to be valid.");
+        }
+
+        byte[] salt = new byte[SALT_LENGTH];
+        byte[] iv = new byte[16];
+        System.arraycopy(fullContent, 0, salt, 0, SALT_LENGTH);
+        System.arraycopy(fullContent, SALT_LENGTH, iv, 0, 16);
+        byte[] encryptedData = new byte[fullContent.length - SALT_LENGTH - 16];
+        System.arraycopy(fullContent, SALT_LENGTH + 16, encryptedData, 0, encryptedData.length);
+
+        SecretKey key = deriveKey(password, salt);
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
+        return cipher.doFinal(encryptedData);
+    }
+
+    public static byte[] decryptSingleFileFromArchive(final File zipFile, final String entryName, final String password) throws Exception {
+        try (FileInputStream fis = new FileInputStream(zipFile);
+             ZipInputStream zis = new ZipInputStream(fis)) {
+
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (!entry.getName().equals(entryName)) continue;
+
+                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                zis.transferTo(buffer);
+                return decryptEntryContent(buffer.toByteArray(), password);
+            }
+        }
+
+        throw new FileNotFoundException("File not found in archive: " + entryName);
+    }
+
     public static Map<String, byte[]> decryptToMemory(final File file, final String password) throws Exception {
-        final Map<String, byte[]> result = new HashMap<>();
+        Map<String, byte[]> result = new HashMap<>();
 
-        try (final FileInputStream fis = new FileInputStream(file)) {
-            final byte[] salt = fis.readNBytes(SALT_LENGTH);
-            final byte[] iv = fis.readNBytes(16);
-            final SecretKey key = deriveKey(password, salt);
-            try (final CipherInputStream cis = createDecryptionStream(fis, key, iv);
-                 final ZipInputStream zis = new ZipInputStream(cis)) {
+        try (FileInputStream fis = new FileInputStream(file);
+             ZipInputStream zis = new ZipInputStream(fis)) {
 
-                ZipEntry entry;
-                while ((entry = zis.getNextEntry()) != null) {
-                    if (!entry.isDirectory()) {
-                        final ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                        zis.transferTo(stream);
-                        result.put(entry.getName(), stream.toByteArray());
-                    }
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.isDirectory()) continue;
+
+                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                zis.transferTo(buffer);
+                byte[] fullContent = buffer.toByteArray();
+
+                try {
+                    result.put(entry.getName(), decryptEntryContent(fullContent, password));
+                } catch (Exception e) {
+                    System.err.println("Failed to decrypt entry: " + entry.getName());
+                    throw e;
                 }
             }
         }
+
         return result;
     }
 
+
     public static void encryptFromMemory(final Map<String, byte[]> files, final File output, final String password) throws Exception {
-        final byte[] salt = new byte[SALT_LENGTH];
-        new SecureRandom().nextBytes(salt);
-        final SecretKey key = deriveKey(password, salt);
-        final byte[] iv = new byte[16];
-        new SecureRandom().nextBytes(iv);
+        try (final FileOutputStream fos = new FileOutputStream(output);
+             final ZipOutputStream zos = new ZipOutputStream(fos)) {
 
-        final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(iv));
+            for (Map.Entry<String, byte[]> entry : files.entrySet()) {
+                final byte[] salt = new byte[SALT_LENGTH];
+                final byte[] iv = new byte[16];
+                new SecureRandom().nextBytes(salt);
+                new SecureRandom().nextBytes(iv);
 
-        try (final FileOutputStream fos = new FileOutputStream(output)) {
-            fos.write(salt);
-            fos.write(iv);
+                final SecretKey key = deriveKey(password, salt);
+                final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(iv));
+                final byte[] encryptedData = cipher.doFinal(entry.getValue());
 
-            try (final CipherOutputStream cos = new CipherOutputStream(fos, cipher);
-                 final ZipOutputStream zos = new ZipOutputStream(cos)) {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                baos.write(salt);
+                baos.write(iv);
+                baos.write(encryptedData);
 
-                for (final Map.Entry<String, byte[]> entry : files.entrySet()) {
-                    final ZipEntry zipEntry = new ZipEntry(entry.getKey());
-                    zos.putNextEntry(zipEntry);
-                    zos.write(entry.getValue());
-                    zos.closeEntry();
-                }
+                ZipEntry zipEntry = new ZipEntry(entry.getKey());
+                zos.putNextEntry(zipEntry);
+                zos.write(baos.toByteArray());
+                zos.closeEntry();
             }
         }
     }
@@ -289,12 +332,6 @@ public final class Pit {
         final KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 256);
         final SecretKey tmp = factory.generateSecret(spec);
         return new SecretKeySpec(tmp.getEncoded(), "AES");
-    }
-
-    public static CipherInputStream createDecryptionStream(final InputStream in, final SecretKey key, final byte[] iv) throws Exception {
-        final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
-        return new CipherInputStream(in, cipher);
     }
 
 }
